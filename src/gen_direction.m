e980e58dfeaba1b612e45b0e88ce330918005e2c
WillonaC <wang_cen1026@163.com>
2019-04-26 10:36:35 +0800

    new src code

--- src/gen_direction.m
+++ src/gen_direction.m
@@ -1,7 +1,9 @@
-for f_num = 1:300
-    fname=[num2str(f_num,'%05d') '.png'];
-    img=imread(['../dataset/test/rgb/' fname]);
-    alpha=imread(['../dataset/test/alpha/' fname]);
+dirPath='F:/[A]project/hairMatting/matting/';
+for f_num = [6 43 111]
+%     fname=[num2str(f_num,'%05d') '.png'];
+    fname=[num2str(f_num,'%d') '_rgb.png'];
+    img=imread([dirPath 'dataset/test2/rgb/' fname]);
+    alpha=imread([dirPath 'dataset/test2/alpha/' fname]);
     for i=1:size(alpha,1)
         for j=1:size(alpha,2)
             if alpha(i,j)==0
@@ -15,6 +17,6 @@ for f_num = 1:300
     % imshow(img);
     return_map = generate_dataX(img,alpha);
     eval(['dir' fname(1:5) '= return_map;']);
-    save(['./dataset/test/direction/' fname '.mat'],['dir' fname(1:5)],'-v7.3','-nocompression') 
-    clear dir*
+    save([dirPath 'dataset/test2/direction/' fname '.mat'],['dir' fname(1:5)],'-v7.3','-nocompression') 
+%     clear dir*
 end
---
+++ src/generate_trimap.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2 as cv
+
+def generate_trimap(alpha):
+    fg = np.array(np.equal(alpha, 255).astype(np.float32))
+    # fg = cv.erode(fg, kernel, iterations=np.random.randint(1, 3))
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3))
+    unknown = np.array(np.not_equal(alpha, 0).astype(np.float32))
+    unknown = cv.dilate(unknown, kernel, iterations=np.random.randint(1, 20))
+    trimap = fg * 255 + (unknown - fg) * 128
+    return trimap.astype(np.uint8)
+
+for imgNum in range(1,301,1):
+	alpha_img=cv.imread('../dataset/test/alpha/%05d.png'%imgNum)
+	trimap_img=generate_trimap(alpha_img)
+	cv.imwrite('../dataset/test/trimap/%05d.png'%imgNum,trimap_img)
